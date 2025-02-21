f301858bc0714893c8a3710133b7f99042a97351
Gustavo Stingelin <gustavo.stingelin@outlook.com>
2025-02-21 19:18:12 -0300

    add init config

--- cmd/subscriber/main.go
+++ cmd/subscriber/main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"bit-armor-sub/internal"
+	"github.com/joho/godotenv"
 	"log"
+	"log/slog"
 	"os"
 	"os/signal"
 )
 
 func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Erro ao carregar .env: %v", err)
+	}
 	endpoint := "tcp://127.0.0.1:5555"
+
+	internal.InitDBPool()
 	go internal.RunZMQSubscriber(endpoint)
 
 	sigterm := make(chan os.Signal, 1)
 	signal.Notify(sigterm, os.Interrupt)
 	<-sigterm
-	log.Println("Shutting down...")
+	slog.Info("Shutting down...")
 	os.Exit(0)
 }
